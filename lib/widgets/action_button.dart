1c801fc3c702c3ba7e39d313d452e6eade8f6166
unknown <anjumarnab050@gmail.com>
2025-09-28 21:06:33 +0600

    final voidCallback onPressed > final Function()? onPressed;

--- lib/widgets/action_button.dart
+++ lib/widgets/action_button.dart
@@ -2,24 +2,42 @@ import 'package:flutter/material.dart';
 
 class ActionButton extends StatelessWidget {
   final String text;
-  final VoidCallback onPressed;
+  final Function()? onPressed;
   final bool isPrimary;
+  final bool isLoading;
 
   const ActionButton({
     super.key,
     required this.text,
     required this.onPressed,
-    this.isPrimary = true, // default = ElevatedButton
+    this.isPrimary = true,
+    this.isLoading = false,
   });
 
   @override
   Widget build(BuildContext context) {
+    final childWidget =
+        isLoading
+            ? const SizedBox(
+              height: 24,
+              width: 24,
+              child: CircularProgressIndicator(
+                strokeWidth: 2,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            )
+            : Text(
+              text,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            );
+
     return SizedBox(
       height: 56,
       child:
           isPrimary
               ? ElevatedButton(
-                onPressed: onPressed,
+                onPressed:
+                    isLoading ? null : () async => await onPressed?.call(),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color(0xFF2ECC71),
                   foregroundColor: Colors.white,
@@ -28,16 +46,11 @@ class ActionButton extends StatelessWidget {
                     borderRadius: BorderRadius.circular(28),
                   ),
                 ),
-                child: Text(
-                  text,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
+                child: childWidget,
               )
               : OutlinedButton(
-                onPressed: onPressed,
+                onPressed:
+                    isLoading ? null : () async => await onPressed?.call(),
                 style: OutlinedButton.styleFrom(
                   foregroundColor: const Color(0xFF4A90E2),
                   side: const BorderSide(color: Color(0xFF4A90E2), width: 1),
@@ -45,13 +58,7 @@ class ActionButton extends StatelessWidget {
                     borderRadius: BorderRadius.circular(28),
                   ),
                 ),
-                child: Text(
-                  text,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
+                child: childWidget,
               ),
     );
   }
