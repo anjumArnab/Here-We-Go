30b7018bd3e5c6bdb530cebbec1070c4016e3dd8
Sakib Anjum Arnab <anjumarnab050@gmail.com>
2025-11-23 17:46:48 +0600

    NotificationService initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:herewego/providers/chat_provider.dart';
-import 'package:herewego/providers/connection_provider.dart';
 import 'package:provider/provider.dart';
+import '../views/homepage.dart';
+import 'providers/chat_provider.dart';
+import 'providers/connection_provider.dart';
 import 'providers/location_provider.dart';
 import 'providers/route_provider.dart';
 import 'providers/user_location_provider.dart';
-import '../views/homepage.dart';
+import '../services/notification_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize notification service
+  await NotificationService().initialize();
+
   runApp(const HereWeGo());
 }
 
