a42396a423aa404bf2fc238ee3a28b7bebd9ff73
Sakib Anjum Arnab <anjumarnab050@gmail.com>
2025-11-25 20:09:12 +0600

    _buildRoutesSection() is removed and route mode is now integrated in the NavigationMetricsPanel

--- lib/widgets/interactive_pane.dart
+++ lib/widgets/interactive_pane.dart
@@ -1,15 +1,14 @@
 // ignore_for_file: unused_local_variable, deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:herewego/widgets/app_chip_button.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
 import 'app_action_button.dart';
 import '../app_theme.dart';
 import '../widgets/app_text_field.dart';
 import '../providers/connection_provider.dart';
-import '../providers/location_provider.dart';
-import '../providers/route_provider.dart';
-import '../models/user_location.dart';
+
 
 class InteractivePane extends StatelessWidget {
   final LatLng? currentLocation;
@@ -21,8 +20,6 @@ class InteractivePane extends StatelessWidget {
   final VoidCallback onDisconnect;
   final bool isExpanded;
   final VoidCallback onToggleExpand;
-  final Function(String) onRouteModeChanged;
-  final VoidCallback onRouteFilterChanged;
   final PageController pageController;
   final int currentPage;
   final ValueChanged<int> onPageChanged;
@@ -38,8 +35,6 @@ class InteractivePane extends StatelessWidget {
     required this.onDisconnect,
     required this.isExpanded,
     required this.onToggleExpand,
-    required this.onRouteModeChanged,
-    required this.onRouteFilterChanged,
     required this.pageController,
     required this.currentPage,
     required this.onPageChanged,
@@ -52,7 +47,7 @@ class InteractivePane extends StatelessWidget {
       left: 10,
       child: Container(
         constraints: BoxConstraints(
-          maxHeight: MediaQuery.of(context).size.height * 0.50,
+          maxHeight: MediaQuery.of(context).size.height * 0.35,
           maxWidth: MediaQuery.of(context).size.width * 0.95,
         ),
         decoration: BoxDecoration(
@@ -76,7 +71,6 @@ class InteractivePane extends StatelessWidget {
                         children: [
                           _buildServerConnectionSection(context),
                           _buildLocationSenderSection(context),
-                          _buildRoutesSection(context),
                         ],
                       ),
                     ),
@@ -91,21 +85,9 @@ class InteractivePane extends StatelessWidget {
   }
 
   Widget _buildHeader() {
-    return Container(
-      padding: EdgeInsets.all(AppTheme.spacingSmall),
-      child: CircleAvatar(
-        backgroundColor: AppTheme.primaryNavy,
-        child: IconButton(
-          icon: Icon(
-            isExpanded ? Icons.expand_more : Icons.expand_less,
-            color: AppTheme.cardWhite,
-            size: 20,
-          ),
-          padding: EdgeInsets.zero,
-          constraints: const BoxConstraints(),
-          onPressed: onToggleExpand,
-        ),
-      ),
+    return AppChipButton(
+      label: isExpanded ? 'Hide Options' : 'Show Options',
+      onTap: onToggleExpand,
     );
   }
 
@@ -117,7 +99,7 @@ class InteractivePane extends StatelessWidget {
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: List.generate(3, (index) {
+        children: List.generate(2, (index) {
           return Container(
             margin: EdgeInsets.symmetric(horizontal: AppTheme.spacingXSmall),
             width: currentPage == index ? 24 : 8,
@@ -322,362 +304,4 @@ class InteractivePane extends StatelessWidget {
     );
   }
 
-  Widget _buildRoutesSection(BuildContext context) {
-    return Consumer3<ConnectionProvider, LocationProvider, RouteProvider>(
-      builder: (context, connectionProvider, locationProvider, routeProvider, _) {
-        final userLocations = locationProvider.userLocations;
-        final currentUserId = connectionProvider.currentUserId;
-
-        final otherUsers =
-            userLocations.entries.where((e) => e.key != currentUserId).toList();
-
-        return SingleChildScrollView(
-          padding: EdgeInsets.symmetric(
-            horizontal: AppTheme.spacingMedium,
-            vertical: AppTheme.spacingSmall,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                children: [
-                  Icon(Icons.route, color: AppTheme.primaryNavy, size: 16),
-                  SizedBox(width: AppTheme.spacingSmall - 2),
-                  Text(
-                    'See where your friends are',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                      color: AppTheme.textDark,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: AppTheme.spacingSmall + 2),
-              _buildRouteModeSelector(context, routeProvider),
-              if (otherUsers.isNotEmpty) ...[
-                SizedBox(height: AppTheme.spacingSmall + 2),
-                _buildMergedShowRoutesToSection(
-                  context,
-                  otherUsers,
-                  routeProvider,
-                ),
-              ] else ...[
-                SizedBox(height: AppTheme.spacingSmall + 2),
-                Container(
-                  padding: EdgeInsets.all(AppTheme.spacingSmall + 2),
-                  decoration: BoxDecoration(
-                    color: AppTheme.gray100,
-                    borderRadius: BorderRadius.circular(AppTheme.radiusSmall),
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.info_outline,
-                        color: AppTheme.gray600,
-                        size: 14,
-                      ),
-                      SizedBox(width: AppTheme.spacingSmall),
-                      Expanded(
-                        child: Text(
-                          'Your friends are not sharing their location yet',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: AppTheme.gray700,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _buildMergedShowRoutesToSection(
-    BuildContext context,
-    List<MapEntry<String, UserLocation>> otherUsers,
-    RouteProvider routeProvider,
-  ) {
-    final isAllSelected = routeProvider.selectedUsers.isEmpty;
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Show Routes To:',
-          style: TextStyle(
-            fontSize: 13,
-            fontWeight: FontWeight.w600,
-            color: AppTheme.textDark,
-          ),
-        ),
-        SizedBox(height: AppTheme.spacingSmall),
-        Wrap(
-          spacing: AppTheme.spacingSmall,
-          runSpacing: AppTheme.spacingSmall,
-          children: [
-            _buildAllUsersChip(
-              context,
-              isAllSelected,
-              otherUsers,
-              routeProvider,
-            ),
-            ...otherUsers.map(
-              (entry) => _buildUserRouteChip(
-                context,
-                entry.key,
-                entry.value,
-                isAllSelected,
-                routeProvider,
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Widget _buildAllUsersChip(
-    BuildContext context,
-    bool isAllSelected,
-    List<MapEntry<String, UserLocation>> otherUsers,
-    RouteProvider routeProvider,
-  ) {
-    return InkWell(
-      onTap: () {
-        if (isAllSelected) {
-          routeProvider.setSelectedUsers(otherUsers.map((e) => e.key).toList());
-        } else {
-          routeProvider.clearSelectedUsers();
-        }
-        onRouteFilterChanged();
-      },
-      borderRadius: BorderRadius.circular(AppTheme.radiusMedium),
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: AppTheme.spacingMedium,
-          vertical: AppTheme.spacingSmall + 2,
-        ),
-        decoration: BoxDecoration(
-          color:
-              isAllSelected
-                  ? AppTheme.primaryGreen.withOpacity(0.15)
-                  : AppTheme.cardWhite,
-          borderRadius: BorderRadius.circular(AppTheme.radiusMedium),
-          border: Border.all(
-            color: isAllSelected ? AppTheme.primaryGreen : AppTheme.gray300,
-            width: isAllSelected ? 2 : 1,
-          ),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              isAllSelected ? Icons.check_box : Icons.check_box_outline_blank,
-              size: 20,
-              color: isAllSelected ? AppTheme.primaryGreen : AppTheme.gray600,
-            ),
-            SizedBox(width: AppTheme.spacingSmall),
-            Text(
-              'Everyone',
-              style: TextStyle(
-                fontSize: 13,
-                fontWeight: isAllSelected ? FontWeight.w600 : FontWeight.w500,
-                color:
-                    isAllSelected ? AppTheme.primaryGreen : AppTheme.textDark,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildUserRouteChip(
-    BuildContext context,
-    String userId,
-    UserLocation location,
-    bool isAllSelected,
-    RouteProvider routeProvider,
-  ) {
-    final isUserSelected =
-        isAllSelected || routeProvider.selectedUsers.contains(userId);
-    final isEnabled = !isAllSelected;
-
-    final routeInfo =
-        currentLocation != null
-            ? routeProvider.getRouteInfo(
-              userId: userId,
-              userLocation: location,
-              currentLocation: currentLocation!,
-            )
-            : null;
-
-    return InkWell(
-      onTap:
-          isEnabled
-              ? () {
-                if (isUserSelected) {
-                  routeProvider.removeSelectedUser(userId);
-                } else {
-                  routeProvider.addSelectedUser(userId);
-                }
-                onRouteFilterChanged();
-              }
-              : null,
-      borderRadius: BorderRadius.circular(AppTheme.radiusMedium),
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: AppTheme.spacingMedium,
-          vertical: AppTheme.spacingSmall + 2,
-        ),
-        decoration: BoxDecoration(
-          color:
-              isUserSelected && isEnabled
-                  ? AppTheme.infoBlue.withOpacity(0.15)
-                  : AppTheme.cardWhite,
-          borderRadius: BorderRadius.circular(AppTheme.radiusMedium),
-          border: Border.all(
-            color:
-                isUserSelected && isEnabled
-                    ? AppTheme.infoBlue
-                    : AppTheme.gray300,
-            width: isUserSelected && isEnabled ? 2 : 1,
-          ),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              isUserSelected ? Icons.check_box : Icons.check_box_outline_blank,
-              size: 20,
-              color:
-                  isEnabled
-                      ? (isUserSelected ? AppTheme.infoBlue : AppTheme.gray600)
-                      : AppTheme.gray400,
-            ),
-            SizedBox(width: AppTheme.spacingSmall),
-            Text(
-              userId,
-              style: TextStyle(
-                fontSize: 13,
-                fontWeight:
-                    isUserSelected && isEnabled
-                        ? FontWeight.w600
-                        : FontWeight.w500,
-                color:
-                    isEnabled
-                        ? (isUserSelected
-                            ? AppTheme.infoBlue
-                            : AppTheme.textDark)
-                        : AppTheme.gray400,
-              ),
-            ),
-            if (routeInfo != null) ...[
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 6),
-                child: Text(
-                  '•',
-                  style: TextStyle(
-                    fontSize: 13,
-                    color: isEnabled ? AppTheme.gray600 : AppTheme.gray400,
-                  ),
-                ),
-              ),
-              Text(
-                routeInfo.lastUpdated,
-                style: TextStyle(
-                  fontSize: 11,
-                  color: isEnabled ? AppTheme.textGray : AppTheme.gray400,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 6),
-                child: Text(
-                  '•',
-                  style: TextStyle(
-                    fontSize: 13,
-                    color: isEnabled ? AppTheme.gray600 : AppTheme.gray400,
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    Icons.straighten,
-                    size: 12,
-                    color: isEnabled ? AppTheme.gray600 : AppTheme.gray400,
-                  ),
-                  SizedBox(width: 3),
-                  Text(
-                    routeInfo.directDistance,
-                    style: TextStyle(
-                      fontSize: 11,
-                      color: isEnabled ? AppTheme.gray700 : AppTheme.gray400,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildRouteModeSelector(
-    BuildContext context,
-    RouteProvider routeProvider,
-  ) {
-    final modes = ['driving', 'walking', 'cycling'];
-    final icons = [
-      Icons.directions_car,
-      Icons.directions_walk,
-      Icons.directions_bike,
-    ];
-
-    return Row(
-      children: List.generate(modes.length, (index) {
-        final mode = modes[index];
-        final isSelected = routeProvider.routeMode == mode;
-
-        return Expanded(
-          child: Padding(
-            padding: EdgeInsets.only(
-              right: index < modes.length - 1 ? AppTheme.spacingSmall - 2 : 0,
-            ),
-            child: InkWell(
-              onTap: () => onRouteModeChanged(mode),
-              borderRadius: BorderRadius.circular(AppTheme.radiusSmall),
-              child: Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: AppTheme.spacingSmall - 2,
-                ),
-                decoration: BoxDecoration(
-                  color: isSelected ? AppTheme.primaryNavy : AppTheme.cardWhite,
-                  borderRadius: BorderRadius.circular(AppTheme.radiusSmall),
-                  border: Border.all(
-                    color: isSelected ? AppTheme.primaryNavy : AppTheme.gray300,
-                    width: isSelected ? 2 : 1,
-                  ),
-                ),
-                child: Icon(
-                  icons[index],
-                  color: isSelected ? AppTheme.cardWhite : AppTheme.gray600,
-                  size: 16,
-                ),
-              ),
-            ),
-          ),
-        );
-      }),
-    );
-  }
 }
