e8638884b37ce86e171cc677e4fd3ffc1404ba7d
Sakib Anjum Arnab <anjumarnab050@gmail.com>
2025-11-25 20:13:58 +0600

    NavigationProvider is initialized

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'providers/connection_provider.dart';
 import 'providers/location_provider.dart';
 import 'providers/route_provider.dart';
 import 'providers/user_location_provider.dart';
+import 'providers/navigation_provider.dart';
 import '../services/notification_service.dart';
 
 void main() async {
@@ -29,6 +30,7 @@ class HereWeGo extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => LocationProvider()),
         ChangeNotifierProvider(create: (_) => RouteProvider()),
         ChangeNotifierProvider(create: (_) => UserLocationProvider()),
+        ChangeNotifierProvider(create: (_) => NavigationProvider()),
         ChangeNotifierProvider(create: (_) => ChatProvider()),
       ],
       child: MaterialApp(
